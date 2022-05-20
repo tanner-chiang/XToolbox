eb43d7205e8ffef53ef703d7e8cc3d8090538628
xemulat <98595166+xemulat@users.noreply.github.com>
2022-05-20 14:29:38 +0200

    Update sdown.py

--- sdown.py
+++ sdown.py
@@ -5,11 +5,9 @@ import colorama
 from colorama import Fore, Back, Style
 colorama.init(autoreset=True)
 
-
 libname = 'LibreWolf 100.0.1 Setup.exe'
 pyname = 'Python 3.10.4 Setup.exe'
-redistname = 'Basic Redists 1.0 SFX.exe'
-
+redistname = 'BasicRedists 1.0 SFX.exe'
 
 print(Fore.BLUE +"[I>] Welcome " + os.getlogin() + "!")
 print(Fore.BLUE +"[I>] This script will download selected software")
@@ -17,7 +15,7 @@ print(Fore.WHITE +"[K>] Do you wanna install python?")
 pyinstall = input("(y/n): ")
 print(Fore.WHITE +"[K>] Do you wanna install LibreWolf?")
 libreinstall = input("(y/n): ")
-print(Fore.WHITE +"[K>] Do you wanna install Redists?")
+print(Fore.WHITE +"[K>] Do you wanna install BasicRedists?")
 redistinstall = input("(y/n): ")
 
 if pyinstall == 'y':
@@ -26,16 +24,25 @@ if pyinstall == 'y':
     print(" ")
     print(Fore.RED +"[S>] Python Downloaded!")
 if redistinstall == 'y':
-    print(Fore.RED +"[S>] Downloading Basic Redists...")
+    print(Fore.RED +"[S>] Downloading BasicRedists...")
     wget.download('https://github.com/xemulat/Basic-Redists/releases/download/1/basic.redists.exe', redistname)
     print(" ")
-    print(Fore.RED +"[S>] Redists downloaded!")
+    print(Fore.RED +"[S>] BasicRedists downloaded!")
 if libreinstall == 'y':
     print(Fore.RED +"[S>] Downloading LibreWolf...")
     wget.download('https://gitlab.com/librewolf-community/browser/windows/uploads/a3bf5f8c45de37868fec4b20984a1442/librewolf-100.0.1-1.en-US.win64-setup.exe', libname)
     print(" ")
     print(Fore.RED +"[S>] LibreWolf downloaded!")
 else:
-    print(Fore.RED +"[S>] All done, exiting...")
+    print(Fore.RED +"[S>] Nothing was done, exiting...")
     time.sleep(2)
     exit()
+if pyinstall == 'y':
+    print(Fore.RED +"[S>] Executing Python Installer...")
+    os.startfile(pyname)
+if redistinstall == 'y':
+    print(Fore.RED +"[S>] Executing BasicRedists Installer...")
+    os.startfile(redistname)
+if libreinstall == 'y':
+    print(Fore.RED +"[S>] Executing LibreWolf Installer...")
+    os.startfile(libname)
