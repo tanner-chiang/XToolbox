0292293288747d0126fa0e6a5d3b60ac3e109486
xemulat <98595166+xemulat@users.noreply.github.com>
2022-06-10 22:26:40 +0200

    Update updater.py

--- updater.py
+++ updater.py
@@ -9,7 +9,7 @@ from colorama import Fore, Back, Style, init
 init(autoreset=True)
 
 #set vars
-yourversion = ('0.8')
+yourversion = ('0.9')
 fname1 = 'relver.txt'
 fname2 = 'Virus.Removal.Toolkit.zip'
 
